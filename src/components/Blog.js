d8c940ed1cf7620c0037a80862cab517ff72581b
trevorhere <trevoranthonylane@gmail.com>
2019-09-01 14:54:07 -0600

    this was the branch were i tried to use json in gisst to add more data, didnt work

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,11 +3,13 @@ import '../App.css';
 import ReactMarkdown from 'react-markdown';
 import { Card, CardTitle, CardPanel, Button, Row, Col } from "react-materialize";
 import { Link } from "react-router-dom"
+import Post from "./Post"
+
 
 
 
 class Blog extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       error: null,
@@ -16,73 +18,98 @@ class Blog extends Component {
     }
   }
 
-  postLink={
-    width:'100%',
-    margin:'0 auto'
+  postLink = {
+    width: '100%',
+    margin: '0 auto'
 
   }
 
-  blog={
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center',
+  blog = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
     paddingTop: '10%'
   }
-  posts={
-    width:'75%',
+  posts = {
+    width: '75%',
   }
 
-  translateTitle(title){
-    return (title.substr(0,(title.length - 3)).split('_').join(' '))
-  }
-
-  componentDidMount(){
-    fetch(`https://api.github.com/gists/68cc754fb298f3121b5b2b4cfaa754d4`)
-    .then(response =>  response.json())
-    .then(data => {
-      let files = []
-      // Loop through files
-      const fileNames = Object.keys(data.files);
-      for (let i = 0; i < fileNames.length; i++) {
-        var fileName = fileNames[i];
-        let content = data.files[fileName].content
-    
-        files.push({"filename":`${fileName}`, "content":`${content}`});
+  renderPost(title, content, date, filename) {
+    this.props.history.push({
+      pathname: `/blog/${filename}/68cc754fb298f3121b5b2b4cfaa754d4`,
+      state: {
+        title,
+        content,
+        date
       }
-      this.setState({ 
-        isLoaded: true, 
-        files
+    })
+  }
+
+  translateTitle(title) {
+    return (title.substr(0, (title.length - 3)).split('_').join(' '))
+  }
+
+  translatePostsData(data) {
+    let posts = []
+    const fileNames = Object.keys(data);
+    for (let i = 0; i < fileNames.length; i++) {
+      var fileName = fileNames[i];
+      let content = data[fileName].content
+
+      let contentObj = JSON.parse(content.substr(1, content.length - 2))
+      contentObj.filename = fileName
+      posts.push(contentObj);
+    }
+    return posts;
+  }
+
+  componentDidMount() {
+    fetch(`https://api.github.com/gists/68cc754fb298f3121b5b2b4cfaa754d4`)
+      .then(response => response.json())
+      .then(data => {
+        let files = this.translatePostsData(data.files)
+        this.setState({
+          isLoaded: true,
+          files
+        })
+      }, (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
       })
-    },(error) => {
-      this.setState({
-        isLoaded: true,
-        error
-      });
-    })  
   }
 
   render() {
     const { error, isLoaded, files } = this.state;
     console.log('files_state: ', files)
-    if(error){
+    if (error) {
       return <div>Error: {error.message}</div>;
-    } else if (!isLoaded){
+    } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
       return (
         <div style={this.blog}>
-        <div style={this.posts} className="Blog">
+          <div style={this.posts} className="Blog">
             <h3>Posts</h3>
-              {files.map(item => { 
-                return (
-                <div style={this.postLink}>
-                  <hr className="Divider Grey"/>
-                  <div  className="PostLink"><b><a  href={`/blog/${item.filename}/68cc754fb298f3121b5b2b4cfaa754d4`} >{this.translateTitle(item.filename)}</a></b></div>
-                  {/* <ReactMarkdown source={item.content} /> */}
-                </div>
-                )
-              })
-              }
+            {files.map(item => {
+              const { title, content, date, filename } = item;
+              return (
+                <Row
+                  style={this.postLink}
+                  onClick={() => this.renderPost(title, content, date, filename)}
+                >
+                  <hr className="Divider Grey" />
+                  <Col s={6}>
+                    {item.title}
+                  </Col>
+                  <Col className="right-align" s={6}>
+                    {item.date}
+                  </Col>
+                </Row>
+              )
+            })
+            }
           </div>
         </div>
       );
--- src/components/Post.js
+++ src/components/Post.js
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom"
 
 
 class Post extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       error: null,
@@ -16,56 +16,60 @@ class Post extends Component {
     }
   }
 
-  translateTitle(title){
-    return (title.substr(0,(title.length - 3)).split('_').join(' '))
+  page = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: '10%'
   }
 
-  componentDidMount(){
+  posts = {
+    width: '100%',
+  }
 
+  translatePostsData(targetFile, content) {
+    let contentObj = JSON.parse(content.substr(1, content.length - 2))
+    contentObj.filename = targetFile
+    console.log('co', contentObj)
+    return contentObj;
+  }
+
+  componentDidMount() {
     const targetFile = this.props.match.params.file_name
     fetch(`https://api.github.com/gists/68cc754fb298f3121b5b2b4cfaa754d4`)
-    .then(response =>  response.json())
-    .then(data => {
-      let post = {"filename": targetFile, "content": data.files[targetFile].content}
-      this.setState({ 
-        isLoaded: true, 
-        post
+      .then(response => response.json())
+      .then(data => {
+        let post = this.translatePostsData(targetFile, data.files[targetFile].content)
+
+        this.setState({
+          isLoaded: true,
+          post
+        })
+      }, (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
       })
-    },(error) => {
-      this.setState({
-        isLoaded: true,
-        error
-      });
-    })  
   }
 
   render() {
     const { error, isLoaded, post } = this.state;
-    if(error){
+    if (error) {
       return <div>Error: {error.message}</div>;
-    } else if (!isLoaded){
+    } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
       return (
-        <div className="Blog">
-          <div className="PostList">
-          <Row>
-            <Col l={6}>
-              <h4>{this.translateTitle(post.filename)}</h4>
-            </Col>
-            <Col l={6}>
-              <Button right styles={{left:"0px"}} flat><Link to="/blog">Posts</Link></Button>
-            </Col>
-
-
-          </Row>
-
-          <ReactMarkdown source={post.content} />
-        </div>
+        <div style={this.page}>
+          <div style={this.post}>
+            <h4>{post.title}</h4>
+            <hr />
+            <ReactMarkdown source={post.content} />
+          </div>
         </div>
       );
     }
   }
 }
-
 export default Post;
