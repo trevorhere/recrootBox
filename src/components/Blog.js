fd42e7fc5754f9652fd9b497c32bbb8393f3b9c8
trevorhere <trevoranthonylane@gmail.com>
2019-09-02 18:52:27 -0600

    adding blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom"
 
 
 class Blog extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       error: null,
@@ -16,73 +16,72 @@ class Blog extends Component {
     }
   }
 
-  postLink={
-    width:'100%',
-    margin:'0 auto'
+  postLink = {
+    width: '100%',
+    margin: '0 auto'
 
   }
 
-  blog={
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center',
+  blog = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
     paddingTop: '10%'
   }
-  posts={
-    width:'75%',
+  posts = {
+    width: '75%',
   }
 
-  translateTitle(title){
-    return (title.substr(0,(title.length - 3)).split('_').join(' '))
+  translateTitle(title) {
+    return (title.substr(0, (title.length - 3)).split('_').join(' '))
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch(`https://api.github.com/gists/68cc754fb298f3121b5b2b4cfaa754d4`)
-    .then(response =>  response.json())
-    .then(data => {
-      let files = []
-      // Loop through files
-      const fileNames = Object.keys(data.files);
-      for (let i = 0; i < fileNames.length; i++) {
-        var fileName = fileNames[i];
-        let content = data.files[fileName].content
-    
-        files.push({"filename":`${fileName}`, "content":`${content}`});
-      }
-      this.setState({ 
-        isLoaded: true, 
-        files
+      .then(response => response.json())
+      .then(data => {
+        let files = []
+        // Loop through files
+        const fileNames = Object.keys(data.files);
+        for (let i = 0; i < fileNames.length; i++) {
+          var fileName = fileNames[i];
+          let content = data.files[fileName].content
+
+          files.push({ "filename": `${fileName}`, "content": `${content}` });
+        }
+        this.setState({
+          isLoaded: true,
+          files
+        })
+      }, (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
       })
-    },(error) => {
-      this.setState({
-        isLoaded: true,
-        error
-      });
-    })  
   }
 
   render() {
     const { error, isLoaded, files } = this.state;
     console.log('files_state: ', files)
-    if(error){
+    if (error) {
       return <div>Error: {error.message}</div>;
-    } else if (!isLoaded){
+    } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
       return (
         <div style={this.blog}>
-        <div style={this.posts} className="Blog">
+          <div style={this.posts} className="Blog">
             <h3>Posts</h3>
-              {files.map(item => { 
-                return (
+            {files.map(item => {
+              return (
                 <div style={this.postLink}>
-                  <hr className="Divider Grey"/>
-                  <div  className="PostLink"><b><a  href={`/blog/${item.filename}/68cc754fb298f3121b5b2b4cfaa754d4`} >{this.translateTitle(item.filename)}</a></b></div>
-                  {/* <ReactMarkdown source={item.content} /> */}
+                  <hr className="Divider Grey" />
+                  <div key={item.fileName} className="PostLink"><b><a href={`/blog/${item.filename}/68cc754fb298f3121b5b2b4cfaa754d4`} >{this.translateTitle(item.filename)}</a></b></div>
                 </div>
-                )
-              })
-              }
+              )
+            })
+            }
           </div>
         </div>
       );
