788c48fe38d13aa31986c00b5ea1ae31082bbafc
Trevor Lane <tlane@simplenexus.com>
2020-02-27 10:26:55 -0700

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,9 +32,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // The property ID; the tracking code won't be generated without it
         trackingId: "UA-69073529-2",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
       }
     },
