f82d7f108c94b93188a7149b49b4ff30bbe3b50d
Trevor Lane <tlane@simplenexus.com>
2020-02-29 00:01:33 -0700

    email

--- src/components/EmailCaptureSection.js
+++ src/components/EmailCaptureSection.js
@@ -52,6 +52,10 @@ const EmailCaptureSection = () => {
             <RbButton text={`Go!`} type="submit"/>
             </form>
         </FormRow>
+        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        {/* You still need to add the hidden input with the form name to your JSX form */}
+        <input type="hidden" name="form-name" value="contact" />
+        </form>
      </Row>
       : null
   )
