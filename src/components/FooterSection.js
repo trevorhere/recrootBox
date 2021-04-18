589a540489134645c9be66d970ab659092fe4765
Trevor <trevoranthonylane@gmail.com>
2021-04-18 00:04:03 -0600

    descriptive link

--- src/components/FooterSection.js
+++ src/components/FooterSection.js
@@ -6,7 +6,7 @@ const FooterSection = () => {
   return (
     showFooter ?
       <div className="FooterSection">
-        <h3>Like the site? Feel free to use it for yourself. You can find it <a href="https://github.com/trevorhere/recrootBox">here</a></h3>
+        <h3>Like the site? Feel free to use it for yourself. You can find it <a href="https://github.com/trevorhere/recrootBox">on github.</a></h3>
       </div>
       : null
   )
